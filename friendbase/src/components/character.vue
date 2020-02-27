cfac94ab24c6dca8012fbd8fa7ad21bf26ccacd4
Pratheeksha R <aditiravi96@gmail.com>
2020-02-27 11:12:02 -0600

    Basic App

--- friendbase/src/components/character.vue
+++ friendbase/src/components/character.vue
@@ -1,6 +1,16 @@
 <template>
-    <div class="">
+    <div class="col-md-4" @click="switchCharacter">
         {{character}}
+        <div class="character-card">
+            <div class="card-block">
+                <h4 class="card-title">{{character.name}}</h4>
+                <p class="card-text">Height: {{character.height}}</p>
+                <p class="card-text">Mass: {{character.mass}}</p>
+                <p class="card-text">Hair Color: {{character.hair_color}}</p>
+                <p class="card-text">Eye Color: {{character.eye_color}}</p>
+
+            </div>
+        </div>
     </div>
 
 </template>
@@ -8,23 +18,28 @@
 <script>
 
 export default {
+    props: ['id'],
     data() {
         return {
             character: {}
         }
     },
     methods: {
-    fetchCharacter() {
-      fetch('https://cors-anywhere.herokuapp.com/https://swapi.co/api/people/1',
+    fetchCharacter(id) {
+      fetch(`https://cors-anywhere.herokuapp.com/https://swapi.co/api/people/${id}`,
       {
-        method: 'GET',
+        method: 'GET'
       })
       .then(response => response.json())
       .then(json => this.character = json)
+    },
+    switchCharacter(id) {
+        let random_id = Math.floor(Math.random() * 83 ) + 1
+        this.fetchCharacter(random_id)
     }
     },
     created(){
-        this.fetchCharacter()
+        this.fetchCharacter(this.id)
     }
 
 }
