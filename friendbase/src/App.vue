1b55069995543e38b8480171fc159e248abe0fa4
Pratheeksha R <aditiravi96@gmail.com>
2020-02-26 22:05:21 -0600

    Incomplete app because of CORS error. Supposed to be FRIENDS

---
+++ friendbase/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h3>{{ title }}</h3>
+<Character />
+  </div>
+</template>
+
+<script>
+import Character from './components/character.vue'
+export default {
+  name: 'app',
+  data () {
+    return {
+      title: 'Generate your team.'
+    }
+  },
+  components: {
+    Character
+  }
+}
+</script>
---
+++ friendbase/src/components/character.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="">
+        {{character}}
+    </div>
+
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            character: {}
+        }
+    },
+    methods: {
+        fetchCharacter() {
+      fetch('http://swapi.co/api/people/1',
+      {
+        mode: 'no-cors'
+      },
+      {
+        method: 'GET'
+      })
+      .then(response => response.json())
+      .then(json => console.log(json))
+    },
+    created() {
+        this.fetchCharacter();
+    }
+    }
+}
+</script>
